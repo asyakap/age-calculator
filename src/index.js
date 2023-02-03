aa458e3169b3b723803e275bdf66a45e5d24cb6c
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-02-03 10:19:39 -0800

    add user interface logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import Calculator from './../src/js/weekday-calculator.js'; 
+import Calculator from './../src/js/age-calculator.js';
 
 function handleFormSubmission() {
-
+  event.preventDefault();
+  const calculate = new Calculator();
+  let age = document.getElementById("inputAge").value;
+  let pastBirthday = document.getElementById("inputPastBirthday").value;
+  let futureBirthday = document.getElementById("inputFutureBirthday").value;
+  document.getElementById("mercuryAge").innerText = calculate.MercuryAge(age);
+  document.getElementById("mercuryPastBirthday").innerText = calculate.PassedYearsMercury(age, pastBirthday);
+  document.getElementById("mercuryFutureBirthday").innerText = calculate.FutureYearsMercury(age, futureBirthday);
+  document.getElementById("venusAge").innerText = calculate.VenusAge(age);
+  document.getElementById("venusPastBirthday").innerText = calculate.PassedYearsVenus(age, pastBirthday);
+  document.getElementById("venusFutureBirthday").innerText = calculate.FutureYearsVenus(age, futureBirthday);
+  document.getElementById("marsAge").innerText = calculate.MarsAge(age);
+  document.getElementById("marsPastBirthday").innerText = calculate.PassedYearsMars(age, pastBirthday);
+  document.getElementById("marsFutureBirthday").innerText = calculate.FutureYearsMars(age, futureBirthday);
+  document.getElementById("jupiterAge").innerText = calculate.JupiterAge(age);
+  document.getElementById("jupiterPastBirthday").innerText = calculate.PassedYearsJupiter(age, pastBirthday);
+  document.getElementById("jupiterFutureBirthday").innerText = calculate.FutureYearsJupiter(age, futureBirthday);
 }
 
 window.addEventListener("load", function () {
-  document.querySelector("form#new-date").addEventListener("submit", handleFormSubmission);
+  document.querySelector("form#age-calculator").addEventListener("submit", handleFormSubmission);
 });
--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -8,72 +8,72 @@ export default class Calculator {
 
   MercuryAge(age) {
     let mercuryAge = age / 0.24;
-    mercuryAge = Math.round((mercuryAge + Number.EPSILON) * 100) / 100
+    mercuryAge = Math.round((mercuryAge + Number.EPSILON) * 100) / 100;
     return mercuryAge;
   }
 
   VenusAge(age) {
     let venusAge = age / 0.62;
-    venusAge = Math.round((venusAge + Number.EPSILON) * 100) / 100
+    venusAge = Math.round((venusAge + Number.EPSILON) * 100) / 100;
     return venusAge;
   }
   
   MarsAge(age) {
     let marsAge = age / 1.88;
-    marsAge = Math.round((marsAge + Number.EPSILON) * 100) / 100
+    marsAge = Math.round((marsAge + Number.EPSILON) * 100) / 100;
     return marsAge;
   }
 
   JupiterAge(age) {
     let jupiterAge = age / 11.86;
-    jupiterAge = Math.round((jupiterAge + Number.EPSILON) * 100) / 100
+    jupiterAge = Math.round((jupiterAge + Number.EPSILON) * 100) / 100;
     return jupiterAge;
   }
 
   PassedYearsMercury(age, pastBirthday) {
     let passedYearsMercury = (age - pastBirthday) / 0.24;
-    passedYearsMercury = Math.round((passedYearsMercury + Number.EPSILON) * 100) / 100
+    passedYearsMercury = Math.round((passedYearsMercury + Number.EPSILON) * 100) / 100;
     return passedYearsMercury;
   }
 
   PassedYearsVenus(age, pastBirthday) {
     let passedYearsVenus = (age - pastBirthday) / 0.62;
-    passedYearsVenus = Math.round((passedYearsVenus + Number.EPSILON) * 100) / 100
+    passedYearsVenus = Math.round((passedYearsVenus + Number.EPSILON) * 100) / 100;
     return passedYearsVenus;
   }
 
   PassedYearsMars(age, pastBirthday) {
     let passedYearsMars = (age - pastBirthday) / 1.88;
-    passedYearsMars = Math.round((passedYearsMars + Number.EPSILON) * 100) / 100
+    passedYearsMars = Math.round((passedYearsMars + Number.EPSILON) * 100) / 100;
     return passedYearsMars;
   }
 
   PassedYearsJupiter(age, pastBirthday) {
     let passedYearsJupiter = (age - pastBirthday) / 11.86;
-    passedYearsJupiter = Math.round((passedYearsJupiter + Number.EPSILON) * 100) / 100
+    passedYearsJupiter = Math.round((passedYearsJupiter + Number.EPSILON) * 100) / 100;
     return passedYearsJupiter;
   }
 
   FutureYearsMercury(age, futureBirthday) {
     let futureYearsMercury = (futureBirthday - age) / 0.24;
-    futureYearsMercury = Math.round((futureYearsMercury + Number.EPSILON) * 100) / 100
+    futureYearsMercury = Math.round((futureYearsMercury + Number.EPSILON) * 100) / 100;
     return futureYearsMercury;
   }
 
   FutureYearsVenus(age, futureBirthday) {
     let futureYearsVenus = (futureBirthday - age) / 0.62;
-    futureYearsVenus = Math.round((futureYearsVenus + Number.EPSILON) * 100) / 100
+    futureYearsVenus = Math.round((futureYearsVenus + Number.EPSILON) * 100) / 100;
     return futureYearsVenus;
   }
 
   FutureYearsMars(age, futureBirthday) {
     let futureYearsMars = (futureBirthday - age) / 1.88;
-    futureYearsMars = Math.round((futureYearsMars + Number.EPSILON) * 100) / 100
+    futureYearsMars = Math.round((futureYearsMars + Number.EPSILON) * 100) / 100;
     return futureYearsMars;
   }
   FutureYearsJupiter(age, futureBirthday) {
     let futureYearsJupiter = (futureBirthday - age) / 11.86;
-    futureYearsJupiter = Math.round((futureYearsJupiter + Number.EPSILON) * 100) / 100
+    futureYearsJupiter = Math.round((futureYearsJupiter + Number.EPSILON) * 100) / 100;
     return futureYearsJupiter;
   }
 
